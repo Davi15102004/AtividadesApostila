b12c5b1ea449b5285f1e84329e2a1cda300da954
Davi15102004 <104361239+Davi15102004@users.noreply.github.com>
2022-06-27 12:12:13 -0300

    Update exerc6.java

--- exerc6.java
+++ exerc6.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
  
             public static void main(String[] args) {    
                 for (int i = 2; i <= 123; i++) {
-                    if( ehPrimo(i) )
+                    if( Primo(i) )
                         System.out.println(i + " é primo.");
                 }    
             }
          
-            private static boolean ehPrimo(int numero) {
+            private static boolean Primo(int numero) {
                 for (int j = 2; j < numero; j++) {
                     if (numero % j == 0)
                         return false;   
