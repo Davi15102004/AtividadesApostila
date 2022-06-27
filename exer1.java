4f53940d586ba8378896207dfa1598f61e9a33e7
Davi15102004 <104361239+Davi15102004@users.noreply.github.com>
2022-06-27 12:19:08 -0300

    Update exer1.java

--- exer1.java
+++ exer1.java
@@ -3,33 +3,16 @@ import java.util.Scanner;
 public class exer1{
     public static void main(String[] args) {
     Scanner  input = new Scanner(System.in);
-    Double h,b,B,a;
+    Double R,b,B,C;
     System.out.println("Digite o valor da altura: ");
-    h = input.nextDouble();
+    R = input.nextDouble();
     System.out.println("Digite o valor da base menor: ");
     b = input.nextDouble();
     System.out.println("Digite o valor da base maior: ");
     B = input.nextDouble();
 
-    a = (h*(b + B))/2;
+    C = (R*(b + B))/2;
 
-    System.out.println("O valor da área do trapézio: "+ a +"cm");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    System.out.println("O valor da área do trapézio: "+ C +"cm");
 }
-
-
-
 }
